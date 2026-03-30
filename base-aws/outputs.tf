c699bdc6830adc3ec4fde6a36e743a1a485377d5
David Han <david@zipline.ai>
2026-03-30 12:54:30 -0400

    remove unused outputs

--- base-aws/outputs.tf
+++ base-aws/outputs.tf
@@ -24,23 +24,3 @@ output "kubeconfig_command" {
   description = "Command to update kubeconfig"
   value       = module.orchestration.kubeconfig_command
 }
-
-output "chronon_metadata_table_name" {
-  description = "Full name of the Chronon metadata DynamoDB table (with prefix)"
-  value       = module.orchestration.chronon_metadata_table_name
-}
-
-output "chronon_metadata_base_name" {
-  description = "Base name of the Chronon metadata table (without prefix)"
-  value       = module.orchestration.chronon_metadata_base_name
-}
-
-output "table_partitions_table_name" {
-  description = "Full name of the table partitions DynamoDB table (with prefix)"
-  value       = module.orchestration.table_partitions_table_name
-}
-
-output "table_partitions_base_name" {
-  description = "Base name of the table partitions table (without prefix)"
-  value       = module.orchestration.table_partitions_base_name
-}
